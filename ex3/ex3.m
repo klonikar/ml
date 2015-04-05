cc81dc9f6a8a87d8c09916caafe9eb8a90cedbbc
Kiran Lonikar <klonikar@yahoo-inc.com>
2015-04-05 14:19:45 +0530

    adding ML course exercises

---
+++ ex3/ex3.m
@@ -0,0 +1,93 @@
+%% Machine Learning Online Class - Exercise 3 | Part 1: One-vs-all
+
+%  Instructions
+%  ------------
+% 
+%  This file contains code that helps you get started on the
+%  linear exercise. You will need to complete the following functions 
+%  in this exericse:
+%
+%     lrCostFunction.m (logistic regression cost function)
+%     oneVsAll.m
+%     predictOneVsAll.m
+%     predict.m
+%
+%  For this exercise, you will not need to change any code in this file,
+%  or any other files other than those mentioned above.
+%
+
+%% Initialization
+clear ; close all; clc
+
+%% Setup the parameters you will use for this part of the exercise
+input_layer_size  = 400;  % 20x20 Input Images of Digits
+num_labels = 10;          % 10 labels, from 1 to 10   
+                          % (note that we have mapped "0" to label 10)
+
+%% =========== Part 1: Loading and Visualizing Data =============
+%  We start the exercise by first loading and visualizing the dataset. 
+%  You will be working with a dataset that contains handwritten digits.
+%
+
+% Load Training Data
+fprintf('Loading and Visualizing Data ...\n')
+
+load('ex3data1.mat'); % training data stored in arrays X, y
+m = size(X, 1);
+
+% Randomly select 100 data points to display
+rand_indices = randperm(m);
+sel = X(rand_indices(1:100), :);
+
+displayData(sel);
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+%% ============ Part 2: Vectorize Logistic Regression ============
+%  In this part of the exercise, you will reuse your logistic regression
+%  code from the last exercise. You task here is to make sure that your
+%  regularized logistic regression implementation is vectorized. After
+%  that, you will implement one-vs-all classification for the handwritten
+%  digit dataset.
+%
+
+fprintf('\nTraining One-vs-All Logistic Regression...\n')
+
+lambda = 0.1;
+% Train on first t examples, then predict later rows(X)-t examples
+t = 50;
+num_samples_per_label = rows(X)/num_labels; % 500 images per digit
+Xt = zeros(t*num_labels, columns(X));
+yt = zeros(t*num_labels,1);
+for i = 1:num_labels
+    Xt((i-1)*t+1:i*t,:) = X(num_samples_per_label*(i-1)+1:num_samples_per_label*(i-1)+t,:);
+    yt((i-1)*t+1:i*t,:) = y(num_samples_per_label*(i-1)+1:num_samples_per_label*(i-1)+t,:);
+
+end
+Xt = X;
+yt = y;
+[all_theta] = oneVsAll(Xt, yt, num_labels, lambda);
+
+displayData(all_theta(:, 2:end));
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+
+%% ================ Part 3: Predict for One-Vs-All ================
+%  After ...
+remainder = num_samples_per_label - t;
+Xremainder = zeros(remainder*num_labels, columns(X));
+yremainder = zeros(remainder*num_labels, 1);
+for i=1:num_labels
+    Xremainder((i-1)*remainder+1:i*remainder,:) = X((i-1)*num_samples_per_label+1+t:i*num_samples_per_label,:);
+    yremainder((i-1)*remainder+1:i*remainder,:) = y((i-1)*num_samples_per_label+1+t:i*num_samples_per_label,:);
+end
+Xremainder = X;
+yremainder = y;
+
+pred = predictOneVsAll(all_theta, Xremainder);
+
+fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == yremainder)) * 100);
+
---
+++ ex6/dataset3Params.m
@@ -0,0 +1,53 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   sigma. You should complete this function to return the optimal C and 
+%   sigma based on a cross-validation set.
+%
+
+% You need to return the following variables correctly.
+C = 1;
+sigma = 0.3;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example, 
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using 
+%        mean(double(predictions ~= yval))
+%
+
+searchValues = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+CTrial = searchValues(1);
+sigmaTrial = searchValues(1);
+model = svmTrain(X, y, CTrial, @(x1, x2) gaussianKernel(x1, x2, sigmaTrial));
+predictions = svmPredict(model, Xval);
+minError = mean(double(predictions ~= yval));
+for i = 1:length(searchValues)
+    for j = 1:length(searchValues)
+        CTrial = searchValues(i);
+        sigmaTrial = searchValues(j);
+        model = svmTrain(X, y, CTrial, @(x1, x2) gaussianKernel(x1, x2, sigmaTrial)); 
+        predictions = svmPredict(model, Xval);
+        predError = mean(double(predictions ~= yval));
+        if(predError < minError)
+            minError = predError;
+            C = CTrial;
+            sigma = sigmaTrial;
+        end
+    end
+end
+
+
+
+
+
+% =========================================================================
+
+end
